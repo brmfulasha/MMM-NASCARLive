e12b7165217d5a3c9f49cd0c401a5f8f8d9fd7f6
brmfulasha <32626828+brmfulasha@users.noreply.github.com>
2025-05-25 21:40:49 -0500

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -7,19 +7,26 @@ module.exports = NodeHelper.create({
     },
 
     socketNotificationReceived: function (notification, payload) {
+        console.log(`Socket notification received: ${notification}`);
         if (notification === "GET_NASCAR_DATA") {
+            console.log("Received GET_NASCAR_DATA with URL:", payload);
             this.fetchData(payload);
+        } else {
+            console.log(`Unhandled notification: ${notification}`);
         }
     },
 
     fetchData: function (jsonUrl) {
         const self = this;
+        console.log("Initiating HTTP GET request to:", jsonUrl);
         axios
             .get(jsonUrl)
             .then(response => {
+                console.log("HTTP GET request successful.");
                 if (!response.data || Object.keys(response.data).length === 0) {
                     throw new Error("Invalid NASCAR data received.");
                 }
+                console.log("Data fetched successfully from NASCAR API.");
                 self.sendSocketNotification("NASCAR_DATA", response.data);
             })
             .catch(error => {
