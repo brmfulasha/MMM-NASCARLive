4a6e99ed25234fe6d6348aefb4d17d64b9cd06c7
brmfulasha <32626828+brmfulasha@users.noreply.github.com>
2025-06-29 17:34:37 -0500

    Update MMM-NASCARLive.js

--- MMM-NASCARLive.js
+++ MMM-NASCARLive.js
@@ -88,8 +88,8 @@ Module.register("MMM-NASCARLive", {
       this.show(1000);
     }
 
-    // Hide if flag_state is 9
-  if (this.flag_state === "9") {
+     // Hide if flag_state is 9 (number or string)
+  if (this.flag_state === 9 || this.flag_state === "9") {
     this.hide(1000);
     return wrapper;
   } else {
